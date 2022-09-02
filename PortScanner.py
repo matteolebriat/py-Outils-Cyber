3c65eae4d4a67de492868ef9013cd016d805d4b2
Mattéo LEBRIAT <matteo.lebriat@ayor.fr>
2022-09-02 16:24:57 +0200

    update

--- PortScanner.py
+++ PortScanner.py
@@ -3,7 +3,11 @@ import pyfiglet
 import sys
 import socket
 from datetime import datetime
+import subprocess
 import re
+
+#clear cmd line
+subprocess.call('clear', shell=True)
 
 #banner of the script
 def banner():
@@ -12,32 +16,56 @@ def banner():
 
 #port scanning 
 def portscan():
-    host = input('Enter the host to be scanned : ')
+    #regex to recognise IPv4 addresses
+    ip_regex = re.compile("^([0-9]{1,3}.){3}.[0-9]{1,3}$")
+    
+    #management of error cases if the user enters something other than ipv4 address
+    while True:
+        host = input('Enter the IPv4 address of the host to be scanned (ex : 192.168.0.105) : ')
+        if ip_regex.search(host):
+            print(f"{host} is valid ip address range")
+            break 
+
+    print("-" * 50)
+    print("Scanning Target: " + host)
+
+    #start time of the script    
+    time1 = datetime.now()
+
+    print("Scanning started at:" + str(time1))
+
+    print("-" * 50)
+    print("-" * 50)
+    
+    port1 =int(input("First port : "))
+    port2 = int(input("End port : "))
     
     print("-" * 50)
-
-    print("Scanning Target: " + host)
-    print("Scanning started at:" + str(datetime.now()))
-
     print("-" * 50)
 
-    range1 = int(input('Port Start : '))
-    range2 = int(input('Port End : '))
-
-    print("-" * 50)
-
-    for port in range (range1, range2):
+    #ports are open on the machine
+    for port in range (port1,port2):
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        socket.setdefaulttimeout(0.25)
         result = s.connect_ex((host, port))
-
+        socket.setdefaulttimeout(0.25)
         if result == 0:
             print("--> Port ",port," : ", socket.getservbyport(int(port)), " OPEN")
             s.close()
+
+    #End time of script
+    time2 = datetime.now()
+    
+    print("-" * 50)
+    print("-" * 50)
+    
+    #Total time of script
+    totalTime = time2 - time1
+    
+    print("Total of Time : ",totalTime)
 
 #execute the script
 def execute():
     banner()
     portscan()
-
+    
 execute()
